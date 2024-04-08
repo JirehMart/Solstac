65ebd7872e33b9665e0090b4c71f67d54e2ff262
JirehMart <jvitos1@upao.edu.pe>
2024-04-07 20:13:52 -0500

    RepositorySolstac

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="flex flex-col items-center p-10">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink> | 
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+ nav a.router-link-exact-active {
+   color: coral;
+ }
+</style>
